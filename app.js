d703652ec2ab8d91c96c165c6c29681b69796e89
aryan bhatt <aryanbhatt982@gmail.com>
2026-04-29 12:00:49 +0530

    code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD Pipeline Working');
+  res.send('CI/CD pipeline is working');
 });
 
 module.exports = app;
