fbd4d23194f2eeaf1918fa4afeb1a51af617abc7
aryan bhatt <aryanbhatt982@gmail.com>
2026-04-29 11:40:21 +0530

    code4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const app = require('../index');
 describe('Unit Test - GET /', () => {
   it('should return correct response', async () => {
     const res = await request(app).get('/');
+    expect(res.text).toBe('Hello CI/CD Pipeline Working');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello Aryan, CI/CD pipeline is working!');
   });
 });
