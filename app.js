05f11202bd65f9c0aebaf3d1f89d678e57d1e4dd
aryan bhatt <aryanbhatt982@gmail.com>
2026-04-29 15:20:36 +0530

    code

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('CI/CD pipeline is working');
 });
 
-// ❌ DO NOT call listen here in tests
+// IMPORTANT: only start server if run directly
 if (require.main === module) {
   app.listen(3000, () => {
     console.log('Server running on port 3000');
