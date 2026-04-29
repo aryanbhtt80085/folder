67f4e63a9bcf34c57b101f9ed46189b2903180bb
aryan bhatt <aryanbhatt982@gmail.com>
2026-04-29 11:11:41 +0530

    code3

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Aryan, CI/CD pipeline is working!');
+  res.send('Hello Aryan, CI/CD pipeline is working!');
 });
 
 module.exports = app;
 
 if (require.main === module) {
-  app.listen(port, '0.0.0.0', () => {
-    console.log(`App running on port ${port}`);
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
 }
