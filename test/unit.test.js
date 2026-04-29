f128faa12da2131485ac8d50f58eb8d31bfdc603
aryan bhatt <aryanbhatt982@gmail.com>
2026-04-29 13:04:49 +0530

    code2

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Unit Test', () => {
-  it('GET / should work', async () => {
+describe('Unit Test - GET /', () => {
+  it('should return correct response', async () => {
     const res = await request(app).get('/');
+
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('CI/CD pipeline is working');
+    expect(res.text).toContain('CI/CD pipeline is working!');
   });
 });
