751603e21b4eb91dd3847e275ea47c03d1404eaa
aryan bhatt <aryanbhatt982@gmail.com>
2026-04-29 13:03:22 +0530

    code2

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const app = require('./app');
 
 const port = 3000;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-module.exports = server;
